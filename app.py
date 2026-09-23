39ee5ca454f96bbd9b2073c6594f24fa8b7af2dc
Someone <saurabh.vishwas@op.iitg.ac.in>
2026-09-24 01:25:49 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+def add (a, b):     # Addition function
+    """Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The sum of a and b.
+    """
+    return a + b
+
+print ("How are you?")
+print (add (5, 3))  # Example usage of the add function
