3418ebf346f260ba9b0d77c465a61ffc19a03bcc
Someone <saurabh.vishwas@op.iitg.ac.in>
2026-09-24 02:48:17 +0530

    Modify app.py in main

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def factorial (n):     # Factorial function
             result *= i
         return result
 
-if __name__ == "__main__":
-    print ("How are you?")
-    print (add (5, 3))  # Example usage of the add function
-    print (factorial (5))  
+
+print (add (5, 3))  # Example usage of the add function
+print (factorial (5))  
