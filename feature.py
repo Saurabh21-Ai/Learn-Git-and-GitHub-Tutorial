7c2fbd0fbf6807af4b24687acd56ff95d15fdf06
Someone <saurabh.vishwas@op.iitg.ac.in>
2026-09-25 17:45:42 +0530

    Add Fibonacci iteration in featute.py

--- feature.py
+++ feature.py
@@ -21,6 +21,21 @@ def fibonacci (n):
     else: 
         return fibonacci (n-1) + fibonacci (n - 2)
 
+def fibo_iterate (n):
+    """ Calculates Fibonacci series using Iteration.
+    Args:
+        n (int): The position in the Fibonacci sequence.
+    Returns:
+        int: The nth Fibonacci number.
+    """
+    y = 0
+    z = 1
+    if n < 1:
+        return ValueError ("Fibonacci sequence is start from 1.")
+    else:
+        for i in range (n):
+            y, z = z, y + z
+        return z
 
 if __name__ == "__main__":
     print (power (2.5, 5))
